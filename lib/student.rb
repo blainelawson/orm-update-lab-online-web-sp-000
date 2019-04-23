7d104045197712d9bfd9e0f13489ee7c0cf4e823
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-23 19:21:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,6 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
     binding.pry
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 end
